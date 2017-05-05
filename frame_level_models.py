bb69abba19fe74b7e1b3b6516da3d0e1a64aa388
nwatts02 <nathan.watts@tufts.edu>
2017-05-05 17:49:26 -0400

    added grid-gru

--- frame_level_models.py
+++ frame_level_models.py
@@ -360,3 +360,42 @@ class GridLstmModel(models.BaseModel):
         model_input=state,
         vocab_size=vocab_size,
         **unused_params)
+
+class GridGruModel(models.BaseModel):
+
+  def create_model(self, model_input, vocab_size, num_frames, **unused_params):
+    """Creates a model which uses a stack of Gated Recurrent Units to represent the video.
+    Args:
+      model_input: A 'batch_size' x 'max_frames' x 'num_features' matrix of
+                   input features.
+      vocab_size: The number of classes in the dataset.
+      num_frames: A vector of length 'batch' which indicates the number of
+           frames for each video (before padding).
+
+    Returns:
+      A dictionary with a tensor containing the probability predictions of the
+      model in the 'predictions' key. The dimensions of the tensor are
+      'batch_size' x 'num_classes'.
+    """
+    gru_size = FLAGS.lstm_cells
+    number_of_layers = FLAGS.lstm_layers
+
+    stacked_grid_gru = tf.contrib.rnn.MultiRNNCell(
+            [
+                tf.contrib.grid_rnn.Grid2GRUCell(
+                    gru_size, forget_bias=1.0, use_peepholes=True, state_is_tuple=False, output_is_tuple=False)
+                for _ in range(number_of_layers)], state_is_tuple=False)
+                
+
+    loss = 0.0
+    outputs, state = tf.nn.dynamic_rnn(stacked_grid_gru, model_input,
+                                       sequence_length=num_frames,
+                                       dtype=tf.float32)
+
+    aggregated_model = getattr(video_level_models,
+                               FLAGS.video_level_classifier_model)
+
+    return aggregated_model().create_model(
+        model_input=state,
+        vocab_size=vocab_size,
+        **unused_params)
